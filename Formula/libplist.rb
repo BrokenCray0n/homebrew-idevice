5b9d71941728588a1d92c6ec7cb517b81888098e
BrokenCray0n <brokencray0nau@gmail.com>
2019-10-13 17:12:41 -0400

    Tap Structure + libplist recipe
    f

---
+++ Formula/libplist.rb
@@ -0,0 +1,20 @@
+class LibPlist < Formula
+	desc "A library to handle Apple Property List format in binary or XML"
+	homepage "http://www.libimobiledevice.org"
+	url "https://github.com/tihmstar/libplist/archive/2.0.0.tar.gz"
+
+	depends_on "make" => :build
+	depends_on "autoconf" => :build
+	depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "pkg-config" => :build
+    depends_on "cython" => :optional
+    depends_on "gcc" => :build
+
+    def install
+    	system "./autogen.sh", "--prefix=#{prefix}"
+    	system "make", "install"
+    end
+
+
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# BrokenCray0n Idevice
+
+## How do I install these formulae?
+`brew install brokencray0n/idevice/<formula>`
+
+Or `brew tap brokencray0n/idevice` and then `brew install <formula>`.
+
+Or install via URL (which will not receive updates):
+
+```
+brew install https://raw.githubusercontent.com/BrokenCray0n/homebrew-idevice/master/Formula/<formula>.rb
+```
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
